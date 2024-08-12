ad52a1f02886d7600b551d85aac1164e89e6945e
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-08-12 16:54:39 -0300

    New animation time

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,48 +1,39 @@
 import { motion } from 'framer-motion';
 
+
+const largura = window.innerWidth;
+
 const cloudVariants = {
   topOne: {
-    initial: { x: '50vw', y: '40vh', scale: 4 },
-    animate: { x: '250vw', y: '40vh', scale: 4 },
+    initial: { x: largura < 674 ? '50vw' : '60vw', y: largura < 674 ? '40vh' : '40vh', scale: 4 },
+    animate: { x: largura < 674 ? '250vw' : '250vw', y: largura < 674 ? '40vh' : '40vh', scale: 6 },
   },
   topTwo: {
-    initial: { x: '50vw', y: '30vh', scale: 5 },
-    animate: { x: '-250vw', y: '30vh', scale: 5 },
+    initial: { x: largura < 674 ? '50vw' : '30vw', y: largura < 674 ? '25vh' : '25vh', scale: 5 },
+    animate: { x: largura < 674 ? '-250vw' : '-250vw', y: largura < 674 ? '25vh' : '25vh', scale: 5 },
   },
   bottomThree: {
-    initial: { x: '50vw', y: '-10vh', scale: 3 },
-    animate: { x: '200vw', y: '-10vh', scale: 3 },
+    initial: { x: largura < 674 ? '50vw' : '50vw', y: largura < 674 ? '-10vh' : '-10vh', scale: 3 },
+    animate: { x: largura < 674 ? '250vw' : '200vw', y: largura < 674 ? '-10vh' : '0vh', scale: 3 },
   },
   bottomFour: {
-    initial: { x: '50vw', y: '-20vh', scale: 2 },
-    animate: { x: '-200vw', y: '-20vh', scale: 2 },
+    initial: { x: largura < 674 ? '50vw' : '50vw', y: largura < 674 ? '-30vh' : '-30vh', scale: 2 },
+    animate: { x: largura < 674 ? '-250vw' : '-200vw', y: largura < 674 ? '-30vh' : '-10vh', scale: 4 },
   }
 };
 
 export default function Welcome() {
-
-  const largura = window.innerWidth;
-
   return (
     <div className="relative z-50">
-      <motion.div
-        className="w-[275px] h-[75px] fixed rounded-full bg-white"
-        variants={cloudVariants.topOne}
-        initial="initial"
-        animate="animate"
-        transition={{ duration: largura < 674 ? 6 : 11, ease: "easeInOut" }}
-      >
-        <div className="relative w-full h-full bg-white rounded-full before:absolute before:-top-12 before:left-10 before:w-28 before:h-24 before:bg-white before:rounded-full before:shadow-[90px_0_0_0px_white]" />
-      </motion.div>
 
       <motion.div
-        className="w-[275px] h-[75px] fixed rounded-full bg-white"
-        variants={cloudVariants.topTwo}
+        className="w-[275px] h-[75px] fixed rounded-full bg-slate-100"
+        variants={cloudVariants.bottomFour}
         initial="initial"
         animate="animate"
-        transition={{ duration: largura < 674 ? 5 : 10, ease: "easeInOut" }}
+        transition={{ duration: largura < 674 ? 7 : 14, ease: "easeInOut" }}
       >
-        <div className="relative w-full h-full bg-white rounded-full before:absolute before:-top-12 before:left-10 before:w-28 before:h-24 before:bg-white before:rounded-full before:shadow-[90px_0_0_0px_white]" />
+        <div className="relative w-full h-full bg-slate-100 rounded-full before:absolute before:-top-12 before:left-10 before:w-28 before:h-24 before:bg-slate-100 before:rounded-full before:shadow-[90px_0_0_0px_#f1f5f9;]" />
       </motion.div>
 
       <motion.div
@@ -54,15 +45,24 @@ export default function Welcome() {
       >
         <div className="relative w-full h-full bg-white rounded-full before:absolute before:-top-12 before:left-10 before:w-28 before:h-24 before:bg-white before:rounded-full before:shadow-[90px_0_0_0px_white]" />
       </motion.div>
-
       <motion.div
         className="w-[275px] h-[75px] fixed rounded-full bg-white"
-        variants={cloudVariants.bottomFour}
+        variants={cloudVariants.topTwo}
         initial="initial"
         animate="animate"
-        transition={{ duration: largura < 674 ? 7 : 14, ease: "easeInOut" }}
+        transition={{ duration: largura < 674 ? 5 : 13, ease: "easeInOut" }}
       >
         <div className="relative w-full h-full bg-white rounded-full before:absolute before:-top-12 before:left-10 before:w-28 before:h-24 before:bg-white before:rounded-full before:shadow-[90px_0_0_0px_white]" />
+      </motion.div>
+
+      <motion.div
+        className="w-[275px] h-[75px] fixed rounded-full bg-slate-100"
+        variants={cloudVariants.topOne}
+        initial="initial"
+        animate="animate"
+        transition={{ duration: largura < 674 ? 6 : 11, ease: "easeInOut" }}
+      >
+        <div className="relative w-full h-full bg-slate-100 rounded-full before:absolute before:-top-12 before:left-10 before:w-28 before:h-24 before:bg-slate-100 before:rounded-full before:shadow-[90px_0_0_0px_#f1f5f9;]" />
       </motion.div>
     </div>
   );
